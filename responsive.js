f1ad01bc57273e2b2696c9d0b90f206e4456dbb3
SRB77 <soumyach144@gmail.com>
2025-04-11 11:29:21 +0530

    some styling changes

--- responsive.js
+++ responsive.js
@@ -1,6 +1,5 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector("body");
-
 buttons.forEach(
     (button)=>{button.addEventListener('click',(e)=>{
         if(e.target.id === 'yellow'){
